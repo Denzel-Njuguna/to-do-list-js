9bd8152b32e0db5b74be115125dafd4f9193420f
Denzel <144164130+Denzel-Njuguna@users.noreply.github.com>
2024-07-02 22:04:35 +0300

    Update to_do_list.js

--- to_do_list.js
+++ to_do_list.js
@@ -1,12 +1,49 @@
-let currenttime= new Date()
- const readline = require('readline');
- let notification = readline.createInterface = ({
-  input:process.stdin,
-  output:process.stdout
- });
-notification.question('Event',(event)=>{
-  notification.question('Time',(Time)=>{
-    console.log
-  })
+import readline from 'readline';
+import http from 'http';
+const notification = readline.createInterface({
+ input:process.stdin,
+ output:process.stdout
+});
+notification.question('Event ',(event)=>{
+ notification.question('sethour ',(sethour)=>{
+    notification.question('setminutes ',(setminute)=>{
+        notification.question('am/pm ',(meridiem)=>{
+
+        meridiem = meridiem;
+            sethour = parseInt(sethour)
+        setminute = parseInt(setminute)
+
+        let currenttime= new Date();
+        let currenthour = currenttime.getHours();
+        let currentminute = currenttime.getMinutes();
+
+        if(meridiem === 'am' && sethour ===12){
+            sethour += 0;
+        }
+        if(meridiem === 'pm' && sethour < 12){
+            sethour +=12;
+        }
+        let temphour,tempminute;
+        if(sethour>=currenthour || currenthour == sethour || (setminute >= currentminute)){
+            temphour = sethour - currenthour;
+            tempminute = setminute - currentminute
+        } else  {
+            temphour = (24 -  currenthour) + sethour;
+            tempminute = (60 - currentminute) + setminute;
+        }
+
+        
+        if(tempminute>60){
+            temphour -1;
+            tempminute = 0;
+        }
+        console.log(event,temphour+":"+tempminute)
+        notification.close();
+        
+
+    })
 })
- console.log (currenttime)
+ })
+})
+
+
