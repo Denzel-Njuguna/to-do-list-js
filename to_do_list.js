3bbc1c07e2b8ad2abfa3b9074138d28247e84064
Denzel <144164130+Denzel-Njuguna@users.noreply.github.com>
2024-07-02 22:02:42 +0300

    Add files via upload

---
+++ to_do_list.js
@@ -0,0 +1,12 @@
+let currenttime= new Date()
+ const readline = require('readline');
+ let notification = readline.createInterface = ({
+  input:process.stdin,
+  output:process.stdout
+ });
+notification.question('Event',(event)=>{
+  notification.question('Time',(Time)=>{
+    console.log
+  })
+})
+ console.log (currenttime)
